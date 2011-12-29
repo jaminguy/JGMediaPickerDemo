8025740ca0e33b691b6a84471eaf6eecb810f0d2
Jamin Guy <jaminguy@gmail.com>
2011-12-29 17:38:38 -0600

    added library

--- JGMediaPickerDemo/ViewController.m
+++ JGMediaPickerDemo/ViewController.m
@@ -8,7 +8,19 @@
 
 #import "ViewController.h"
 
+#import <MediaPlayer/MediaPlayer.h>
+
+#import "JGMediaPickerController.h"
+
+@interface ViewController ()
+
+@property (nonatomic, retain) JGMediaPickerController *mediaPickerController;
+
+@end
+
 @implementation ViewController
+
+@synthesize mediaPickerController;
 
 - (void)didReceiveMemoryWarning
 {
@@ -55,6 +67,24 @@
 {
     // Return YES for supported orientations
     return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+- (IBAction)showMediaButtonTouchUpInside:(id)sender {
+    self.mediaPickerController = [[[JGMediaPickerController alloc] init] autorelease];
+    self.mediaPickerController.delegate = self;
+    [self presentModalViewController:self.mediaPickerController animated:YES];
+}
+
+- (void)jgMediaPicker:(JGMediaPickerController *)mediaPicker didPickMediaItems:(MPMediaItemCollection *)mediaItemCollection selectedItem:(MPMediaItem *)selectedItem {
+    [[MPMusicPlayerController iPodMusicPlayer] setQueueWithItemCollection:mediaItemCollection];
+    [[MPMusicPlayerController iPodMusicPlayer] setNowPlayingItem:selectedItem];
+    [[MPMusicPlayerController iPodMusicPlayer] play];
+    
+    [self dismissModalViewControllerAnimated:YES];
+}
+
+- (void)jgMediaPickerDidCancel:(JGMediaPickerController *)mediaPicker {
+    [self dismissModalViewControllerAnimated:YES];
 }
 
 @end
